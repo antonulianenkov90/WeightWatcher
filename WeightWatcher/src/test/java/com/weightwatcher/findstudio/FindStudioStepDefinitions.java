30bc9af8216c81e6086ceb1e65b7baa653dccac7
Anton <Anton@DESKTOP-R9EDG58.home>
2019-05-24 12:30:03 -0400

    Second

--- WeightWatcher/src/test/java/com/weightwatcher/findstudio/FindStudioStepDefinitions.java
+++ WeightWatcher/src/test/java/com/weightwatcher/findstudio/FindStudioStepDefinitions.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.weightwatcher.poms.HomePage;
+import com.weightwatcher.poms.StudioPage;
+import com.weightwatcher.poms.StudioSearchPage;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -23,6 +27,9 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 public class FindStudioStepDefinitions {
 	WebDriver driver;
+	HomePage objHomePage;
+	StudioSearchPage objStudioSearchPage;
+	StudioPage objStudioPage;  
 	String selectedName;
 
     @Before
@@ -35,37 +42,38 @@ public class FindStudioStepDefinitions {
 	
     @Given("^User on home page$")
     public void user_on_home_page() throws Throwable {
-        driver.get("https://www.weightwatchers.com/us/");      
-        assertTrue(driver.getTitle().contains("Weight Loss"));
+        driver.get("https://www.weightwatchers.com/us/");  
+        objHomePage = new HomePage(driver);
+        assertTrue(objHomePage.getHomePageTitle().contains("Weight Loss"));
         
     }
 
     @When("^Click on find a studio$")
     public void click_on_find_a_studio() throws Throwable {
-    driver.findElement(By.linkText("Find a Studio")).click();    
+    	objHomePage.clickFindStudioButton();    
     }
 
     @Then("^Displayed location name matches with the name of the first searched result$")
     public void displayed_location_name_matches_with_the_name_of_the_first_searched_result() throws Throwable {
-    	String displayedName=driver.findElement(By.xpath("//div[@class=\"location__name\"]//span")).getText();
-    	assertEquals(selectedName, displayedName);
-    	System.out.println("TODAY’s hours of operation:\n" + driver.findElement(By.xpath("//div[@class=\"hours-list-item-wrapper hours-list--currentday\"]//div[@class=\"hours-list-item-hours\"]")).getText());
+    	objStudioPage=new StudioPage(driver);
+    	
+    	assertEquals(selectedName, objStudioPage.getStudioName());
+    	System.out.println("TODAY’s hours of operation:\n"+objStudioPage.getTodaySchedule());
     }
 
     @And("^In the search field, search for meetings for zip code \"([^\"]*)\"$")
     public void in_the_search_field_search_for_meetings_for_zip_code_something(String zipcode) throws Throwable {
-    	assertTrue(driver.getTitle().contains("Meetings Near You"));
-    	driver.findElement(By.xpath("//*[@id=\"meetingSearch\"]")).sendKeys(zipcode);
-    	driver.findElement(By.xpath("//*[@id=\"content\"]//button")).click();
+    	objStudioSearchPage=new StudioSearchPage(driver);
+    	assertTrue(objStudioSearchPage.getStudioPageTitle().contains("Meetings Near You"));
+    	objStudioSearchPage.zipCodeSearch(zipcode);
     }
 
     @And("^Click on the first search result$")
     public void click_on_the_first_search_result() throws Throwable {
-    	selectedName =driver.findElement(By.xpath("//*[@class=\"meeting-locations-list__item\"]//div[@class=\"location__name\"]//span")).getText();
+    	selectedName=objStudioSearchPage.getFirstStudioName();
     	System.out.println("Title of first result: "+selectedName);
-    	System.out.println("Distance: "+driver.findElement(By.xpath("//*[@class=\"meeting-locations-list__item\"]//*[@class=\"location__distance\"]")).getText());
-    	driver.findElement(By.xpath("//*[@class=\"meeting-locations-list__item\"][1]")).click();
-    	
+    	System.out.println("Distance: "+objStudioSearchPage.getFirstStudioDistance());
+    	objStudioSearchPage.clickFirstStudio();
     }
     
     @After
